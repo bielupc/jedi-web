62086ade7bf07c9ecb7a121995c2e0c6076c2da2
Biel Altimira <biel.altimira@estudiantat.upc.edu>
2023-02-03 19:01:57 +0100

    estructura principal del projecte

---
+++ projecte_final/proj/scripts/test.js
@@ -0,0 +1,9 @@
+$(window).on("load", async () => {
+  try{
+    const cocktail = (await axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")).data;
+    console.log(cocktail)
+  }
+  catch(error){
+    console.log(error);
+  }
+});
