9f2c25fcab2df70fa58d798972a3a22c100e7e3a
Biel Altimira <biel.altimira@estudiantat.upc.edu>
2023-02-04 23:51:00 +0100

    json server created

--- projecte_final/Bittered Sling/scripts/login.js
+++ projecte_final/Bittered Sling/scripts/login.js
@@ -2,6 +2,7 @@ let mode = "login";
 
 $(window).on("load", async () => {
   $("#change-to-register").on("click", changeToRegister);
+  $("#login").on("click", login);
 });
 
 const changeToRegister = () => {
@@ -11,15 +12,16 @@ const changeToRegister = () => {
       <h1><b>Register</b></h1>
       <h3>Enter your data</h3>
       <form class="login-form">
-        <input required type="text" placeholder="Username"/>
-        <input required type="password" placeholder="Password"/>
-        <input required type="password" placeholder="Repeat password"/>
+        <input class="username" required type="text" placeholder="Username"/>
+        <input class="password-1" required type="password" placeholder="Password"/>
+        <input class="password-2" required type="password" placeholder="Repeat password"/>
         <a id="change-to-login">I'm already registered.</a>
-        <button type="submit" class="btn btn-primary btn-lg">Login</button>
+        <button type="button" id="register" class="btn btn-primary btn-lg">Register</button>
       </form>
     </div>
   `);
   $("#change-to-login").on("click", changeToLogin);
+  $("#register").on("click", register);
 };
 
 const changeToLogin = () => {
@@ -30,12 +32,26 @@ const changeToLogin = () => {
       <h1><b>Login</b></h1>
       <h3>Enter your credentials</h3>
       <form class="login-form">
-        <input required type="text" placeholder="Username"/>
-        <input required type="password" placeholder="Password"/>
+        <input class="username" required type="text" placeholder="Username"/>
+        <input class="password" required type="password" placeholder="Password"/>
         <a id="change-to-register">Are you new here?</a>
-        <button type="submit" class="btn btn-primary btn-lg">Login</button>
+        <button type="button" id="login" class="btn btn-primary btn-lg">Login</button>
       </form>
     </div>
   `);
   $("#change-to-register").on("click", changeToRegister);
+  $("#login").on("click", login);
 };
+
+const login = () => {
+  const username = $(".username").val()
+  const password = $(".password").val()
+  console.log(username, password)
+}
+
+const register = () => {
+  const username = $(".username").val()
+  const password1 = $(".password-1").val()
+  const password2 = $(".password-2").val()
+  console.log(username, password1, password2)
+}
